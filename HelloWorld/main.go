3cda8a0d7c80ec832091b6048be504a097f5b340
MichaelPasqua903 <62964900+MichaelPasqua903@users.noreply.github.com>
2021-08-08 15:17:54 +0200

    Add comments related to hello world program

--- HelloWorld/main.go
+++ HelloWorld/main.go
@@ -1,6 +1,11 @@
 package main
 
+//package==project==workspace
+//Main package is an executable one and must contain a func called main.
+
 import "fmt"
+
+//Imports are necessary to access  other packages
 
 func main() {
 	fmt.Println("Hello World")
