095cd7c76a644cfe7c431f0906631075a63d1f79
MichaelPasqua903 <62964900+MichaelPasqua903@users.noreply.github.com>
2021-08-13 18:46:26 +0200

    A simple program to present arrays and slices

---
+++ ArrayAndSlice/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	//We declare a slice of numbers
+	numbers := []int{1, 2, 3}
+	//We add a number using the function append(slice, newItem)
+	numbers = append(numbers, 4)
+
+	//for loop to iterate on the slice previously created
+	for i, numbers := range numbers {
+		fmt.Println(i, numbers)
+	}
+
+	//We declare an array of strings
+	signs := [3]string{"Gemini", "Leo", "Cancer"}
+
+	for i, signs := range signs {
+		fmt.Println(i, signs)
+	}
+}
